60aea982916bfe90d414ad7bbc4885b65d9f76b8
L.M <l_mcruz98@live.com>
2023-03-01 20:56:29 -0300

    Inicialização do projeto api pagamentos

---
+++ src/test/java/com/lcruz8/apipagamentos/ApiPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcruz8.apipagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
