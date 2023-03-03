bb7c8d7d0b0147f3adc2292c4cfbdcb1bb94ec26
L.M <l_mcruz98@live.com>
2023-03-03 13:41:24 -0300

    Adição de tipos e status

---
+++ src/main/java/com/lcruz8/model/PagamentoStatus.java
@@ -0,0 +1,10 @@
+package com.lcruz8.model;
+
+public final class PagamentoStatus {
+    public static final String PROC_PENDENTE = "PENDENTE";
+    public static final String PROC_FALHA = "FALHA";
+    public static final String PROC_SUCESSO = "SUCESSO";
+
+    private PagamentoStatus(){}
+    
+}
---
+++ src/main/java/com/lcruz8/model/PagamentoTipo.java
@@ -0,0 +1,10 @@
+package com.lcruz8.model;
+
+public final class PagamentoTipo {
+    public static final String PG_PIX = "pix";
+    public static final String PG_CARTAO_CREDITO = "cartao_credito";
+    public static final String PG_CARTAO_DEBITO = "cartao_debito";
+    public static final String PG_BOLETO = "boleto";
+
+    private PagamentoTipo() {}
+}
